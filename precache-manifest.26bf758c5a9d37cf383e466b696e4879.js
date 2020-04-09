63e9bfe5f26d60571ba797b35a2271d8a853a20c
ai73aaa <1qazxcvBG>
2020-04-09 15:24:40 +0300

    Updates

---
+++ precache-manifest.26bf758c5a9d37cf383e466b696e4879.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1ba741c42902aed09272ec9cc886d024",
+    "url": "/neko-shop/index.html"
+  },
+  {
+    "revision": "69d23c618fa37521828b",
+    "url": "/neko-shop/static/css/2.826c19ff.chunk.css"
+  },
+  {
+    "revision": "4d4140941007928a8ee4",
+    "url": "/neko-shop/static/css/main.2fb0856e.chunk.css"
+  },
+  {
+    "revision": "69d23c618fa37521828b",
+    "url": "/neko-shop/static/js/2.b9bf2c01.chunk.js"
+  },
+  {
+    "revision": "86afaa925e0f432774bf95b19b6933fb",
+    "url": "/neko-shop/static/js/2.b9bf2c01.chunk.js.LICENSE"
+  },
+  {
+    "revision": "4d4140941007928a8ee4",
+    "url": "/neko-shop/static/js/main.3185ea2d.chunk.js"
+  },
+  {
+    "revision": "36881853dc79dc988d6c",
+    "url": "/neko-shop/static/js/runtime-main.6e844748.js"
+  }
+]);
